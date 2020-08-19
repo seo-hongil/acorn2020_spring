95d79e2b7fb51e5fece0f89ffd962e319af24be3
hongil <shi8295@naver.com>
2020-08-19 18:30:20 +0900

    댓글 기능 추가

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/dao/CafeCommentDaoImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/dao/CafeCommentDaoImpl.java
@@ -15,9 +15,9 @@ public class CafeCommentDaoImpl implements CafeCommentDao{
 	private SqlSession session;
 	
 	@Override
-	public List<CafeCommentDto> getList(int ref_group) {
+	public List<CafeCommentDto> getList(CafeCommentDto dto) {
 		
-		return session.selectList("cafeComment.getList", ref_group);
+		return session.selectList("cafeComment.getList", dto);
 	}
 
 	@Override
@@ -40,4 +40,17 @@ public class CafeCommentDaoImpl implements CafeCommentDao{
 	public void update(CafeCommentDto dto) {
 		session.update("cafeComment.update", dto);
 	}
+
+	@Override
+	public CafeCommentDto getData(int num) {
+		
+		return session.selectOne("cafeComment.getData", num);
+	}
+
+	@Override
+	public int getCount(int ref_group) {
+		
+		return session.selectOne("cafeComment.getCount", ref_group);
+	}
+
 }
